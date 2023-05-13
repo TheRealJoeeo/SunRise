596259f117339a16b193b72a1b262b101a2ffc2b
ThePapayaMuffin <133167825+ThePapayaMuffin@users.noreply.github.com>
2023-05-13 00:05:09 -0400

    Test

--- LearnToFlappyBird/Assets/BirdScript.cs
+++ LearnToFlappyBird/Assets/BirdScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    //First, it's necessary that we refrence other parts of the game object like the rigid body 2D, since it only auto-recognizes Transform
+
+    public Rigidbody2D myRigidbody; // Like declaring a instance variable in java, with the Unity editor we can make this equal to something easily
+    public float flapStrength; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,9 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) == true) 
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength; //Vector used to move the object, Vector2 is a method that easily does a vector of (0,1), and when multiplied it is a good choice
+        }
     }
 }
