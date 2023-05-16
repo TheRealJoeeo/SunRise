cda9806c6792ba0e7dd7b2a1eaa185ff1fe3f14a
Joeeo <106675353+TheRealJoeeo@users.noreply.github.com>
2023-05-16 12:22:57 -0400

    What
    Things are not working; Made the character move, but now we can't get collisions with the item work

---
+++ SunRise/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+
+        x *= Time.deltaTime;
+        y *= Time.deltaTime;
+
+        transform.Translate(x, y, 0);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+       Debug.Log("work please");
+    }
+}
