7f2fb1696d42925d8feba28fa270b40141c8a25b
Joeeo <106675353+TheRealJoeeo@users.noreply.github.com>
2023-05-18 14:26:16 -0400

    Foliage Update | v0.0.1b
    Prudhvi added trees yay
    now trees spawn with colliders
    got the basis of map generation done
    also changed placement of canvas objects so it scales to different screen sizes
    added comments to scripts

--- SunRise/Assets/Scripts/PickUppable.cs
+++ SunRise/Assets/Scripts/PickUppable.cs
@@ -4,39 +4,63 @@ using UnityEngine;
 
 public class PickUppable : MonoBehaviour
 {
+    // boolean to detect if touching player
     private bool touching = false;
+    // boolean to ensure the thingy in the update loop only runs once
     private bool ranAlready = false;
+    // get the canvas object that shows the "press f to pick up" message
     private GameObject dialogue;
 
+    //runs once at start
     void Start()
     {
+        // find the object, since we can't just do it in the inspector since these things are prefabs
         dialogue = GameObject.Find("PickUpDialogue");
     }
 
+    // runs every frame
     void Update() {
+        // this only runs once becuase of the boolean
         if (!ranAlready) {
+            // makes the dialogue hidden at start; done here becuase for some reason it was setting the object to null when doing it at start; probably some initialization problem
             dialogue.SetActive(false);
+            // make it only run once
             ranAlready = true;
         }
-        if(Input.GetKeyDown (KeyCode.F))
+        // check if touching the player
+        if (touching)
         {
-          if (touching)
-            Destroy(gameObject);
+            // check if F is pressed when touching player
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                // commit die (delete myself)
+                Destroy(gameObject);
+                // to add: do the thing to add it to inventory
+            }
         }
     }
   
+    // when an object enters this collider
      void OnTriggerEnter2D(Collider2D other) {
+         // check if that object is player
          if (other.tag == "Player") {
+            // j debug
             Debug.Log("Press F to pick up");
+            // get the dialogue thingy, make sure it isnt null so no error, then make it show
             if (dialogue != null) dialogue.SetActive(true);
+            // update the touching property to true, becuase now its touching
             touching = true;
         }
      }
      
+     // once that object is not in the collider
      void OnTriggerExit2D(Collider2D other) {
+         // check if player
          if (other.tag == "Player") {
             Debug.Log("Can't anymore");
+            // hide the dialgoue
             if (dialogue != null) dialogue.SetActive(false);
+            // update the is touching property
             touching = false;
         }
      }
--- SunRise/Assets/Scripts/SpawnControl.cs
+++ SunRise/Assets/Scripts/SpawnControl.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class SpawnControl : MonoBehaviour
 {
     public GameObject itemFab;
+    public GameObject treeFab;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < 10; i++)
             item();
+        for (int i= 0; i < 100; i++)
+            tree();
     }
 
     // Update is called once per frame
@@ -22,5 +25,9 @@ public class SpawnControl : MonoBehaviour
     void item() // add paramter for what item is in the object (later)
     {
         Instantiate(itemFab, new Vector3(Random.Range(-25, 25), Random.Range(-25, 25), 0), transform.rotation);
+    }
+    void tree() // add paramter for what item is in the object (later)
+    {
+        Instantiate(treeFab, new Vector3(Random.Range(-18, 18), Random.Range(-18, 18), 0), transform.rotation);
     }
 }
