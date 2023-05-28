00f11a2794db15fdaedcec6c146e64f1a15af7d2
Joeeo <106675353+TheRealJoeeo@users.noreply.github.com>
2023-05-28 13:20:45 -0400

    Small Update for v0.0.5
    - consolidated spawn functions into one function for later
    - working on making spawn control so overlap will be corrected for
    * plan is to spawn structures first, then the rest of the things, that way the rest of the things spawn around the structures (since structures are bigger and therefore need more space to begin with)

--- SunRise/Assets/Scripts/SpawnControl.cs
+++ SunRise/Assets/Scripts/SpawnControl.cs
@@ -12,14 +12,14 @@ public class SpawnControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i= 0; i < 25; i++)
-            tree();
         for (int i = 0; i < 25; i++)
-            rock();
+            spawn(treeFab, -22.5f, 22.5f, -1, true, 1000);
         for (int i = 0; i < 25; i++)
-            crate();
+            spawn(rockFab, -22.5f, 22.5f, 0, true, 1000);
+        for (int i = 0; i < 25; i++)
+            spawn(crateFab, -22.5f, 22.5f, 1, false, 1000);
         for (int i = 0; i < 3; i++)
-            item();
+            spawn(itemFab, -22.5f, 22.5f, 1, false, 1000);
     }
 
     // Update is called once per frame
@@ -28,22 +28,30 @@ public class SpawnControl : MonoBehaviour
         
     }
 
-    void item()
+    private void spawn(GameObject obj, float start, float end, float layer, bool randAngle, float tries)
     {
-        GameObject temp = Instantiate(itemFab, new Vector3(Random.Range(-22.5f, 22.5f), Random.Range(-22.5f, 22.5f), 1), transform.rotation);
-    }
-    void tree() 
-    {
-        Quaternion tempRandAngle = Quaternion.Euler(0, 0, Random.Range(0.0f, 360.0f));
-        Instantiate(treeFab, new Vector3(Random.Range(-22.5f, 22.5f), Random.Range(-22.5f, 22.5f), -1), tempRandAngle);
-    }
-    void rock() 
-    {
-        Quaternion tempRandAngle = Quaternion.Euler(0, 0, Random.Range(0.0f, 360.0f));
-        Instantiate(rockFab, new Vector3(Random.Range(-22.5f, 22.5f), Random.Range(-22.5f, 22.5f), 0), tempRandAngle);
-    }
-    void crate()
-    {
-        Instantiate(crateFab, new Vector3(Random.Range(-22.5f, 22.5f), Random.Range(-22.5f, 22.5f), 1), transform.rotation);
+        Quaternion tempRandAngle;
+        if (randAngle) tempRandAngle = Quaternion.Euler(0, 0, Random.Range(0.0f, 360.0f));
+        else tempRandAngle = transform.rotation;
+
+        GameObject temp = Instantiate(obj, new Vector3(Random.Range(start, end), Random.Range(start, end), layer), tempRandAngle);
+
+        Physics2D.SyncTransforms();
+        Debug.Log(temp.GetComponent<Rigidbody2D>().IsTouchingLayers());
+        Physics2D.SyncTransforms();
+
+        if (temp.GetComponent<Collider2D>().IsTouchingLayers())
+        {
+            Debug.Log("ReAdjusting");
+            if (tries <= 0) // prevent stack overflow
+            {
+                Debug.Log("Failed to find a clear space in the tries given");
+                return;
+            }
+            else
+            {
+                spawn(obj, start, end, layer, randAngle, tries--);
+            }
+        }
     }
 }
